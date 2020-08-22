bde70b62dd3126822fde381555a55367c2522a51
Apriyanto J.P.L Tobing <apriyantotobing8@gmail.com>
2020-08-22 17:00:33 +0700

    Updates

---
+++ precache-manifest.f51faac8e54678e04acbd76839108f8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "124d50e207d82385c5a734c147137391",
+    "url": "/aprijplt/index.html"
+  },
+  {
+    "revision": "144efe0cbdd9606df343",
+    "url": "/aprijplt/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bd2b0f5900e806a7708a",
+    "url": "/aprijplt/static/js/2.782cdc7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aprijplt/static/js/2.782cdc7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "144efe0cbdd9606df343",
+    "url": "/aprijplt/static/js/main.06a65ca8.chunk.js"
+  },
+  {
+    "revision": "b82305b07ad6ca367b9a",
+    "url": "/aprijplt/static/js/runtime-main.f44b5785.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/aprijplt/static/media/logo.5d5d9eef.svg"
+  }
+]);
