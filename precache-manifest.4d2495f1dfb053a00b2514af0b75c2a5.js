b35448e0c613d31818e0449ea252f49661a459a1
Apriyanto J.P.L Tobing <apriyantotobing8@gmail.com>
2020-11-25 11:18:04 +0700

    Updates

---
+++ precache-manifest.4d2495f1dfb053a00b2514af0b75c2a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c27678c981f6766fcb34b0846a28b0",
+    "url": "/aprijplt/index.html"
+  },
+  {
+    "revision": "497c0e2778704cb3258f",
+    "url": "/aprijplt/static/css/main.410a8ab4.chunk.css"
+  },
+  {
+    "revision": "b824b4e4b9633f7647d7",
+    "url": "/aprijplt/static/js/2.33be273b.chunk.js"
+  },
+  {
+    "revision": "d0b010172984b37634e02a95243a36f2",
+    "url": "/aprijplt/static/js/2.33be273b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "497c0e2778704cb3258f",
+    "url": "/aprijplt/static/js/main.ad0fa802.chunk.js"
+  },
+  {
+    "revision": "b82305b07ad6ca367b9a",
+    "url": "/aprijplt/static/js/runtime-main.f44b5785.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aprijplt/precache-manifest.391cf94781269adb9500563611cbeabd.js"
+  "/aprijplt/precache-manifest.4d2495f1dfb053a00b2514af0b75c2a5.js"
 );
 
 self.addEventListener('message', (event) => {
